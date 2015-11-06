cdc6dd54204addab09535a3a259e8f61fea3060f
James <jcl@pml.ac.uk>
2015-11-06 16:36:46 +0000

    Remove python barycentric coordinate calculation.

--- src/model_factory.py
+++ src/model_factory.py
@@ -266,25 +266,6 @@ def get_model_factory(config):
     else:
         raise ValueError('Unsupported ocean circulation model.')    
 
-def get_natural_coords(x, y, x_nodes, y_nodes):
-    # Array entries
-    a11 = y_nodes[2] - y_nodes[0]
-    a12 = x_nodes[0] - x_nodes[2]
-    a21 = y_nodes[0] - y_nodes[1]
-    a22 = x_nodes[1] - x_nodes[0]
-
-    # Determinant
-    a = np.array([[a11,a12],[a21,a22]])
-    det = np.linalg.det(a)
-
-    # Transformation to natural coordinates
-    phi = np.empty(3, dtype='float')
-    phi[0] = (a11*(x - x_nodes[0]) + a12*(y - y_nodes[0]))/det
-    phi[1] = (a21*(x - x_nodes[0]) + a22*(y - y_nodes[0]))/det
-    phi[2] = 1.0 - phi[0] - phi[1]
-    
-    return phi
-
 def interpolate_within_element(var, phi):
     return var[0] + phi[0] * (var[1] - var[0]) + phi[1] * (var[2] - var[0])
 
