5c2863f123697e512f7ae64d34eba35d70122f0d
James <jcl@pml.ac.uk>
2015-11-18 21:19:11 +0000

    Rename profiling script.

--- profiling/profile.py
+++ profiling/profile.py
@@ -1,11 +1,9 @@
 import subprocess
 import pstats, cProfile
 
-import main
-
 profile_out = "profile.prof"
 
-subprocess.call(["python", "-m", "cProfile", "-o", profile_out, "main.py", "-c", "pylag.cfg"])
+subprocess.call(["python", "-m", "cProfile", "-o", profile_out, "prof_main.py", "-c", "pylag.cfg"])
 
 s = pstats.Stats(profile_out)
 s.strip_dirs().sort_stats("time").print_stats(20)
