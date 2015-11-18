034e335067dc38306cf7ddba141bacecac4cd4a5
James <jcl@pml.ac.uk>
2015-11-18 19:42:25 +0000

    Update build.

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name="PyLag",
+setup(name="pylag",
       version="X.X",
       description="Python/Cython Lagrangian Modelling Frmaework",
       author="Jame Clark (PML)",
       author_email="jcl@pml.ac.uk",
-      url="TODO",
-      ext_modules=cythonize(["time_manager.pyx", "particle.pyx", "fvcom_data_reader.pyx", "integrator.pyx"]),
+      url="gitlab.ecosystem-modelling.pml.ac.uk:jimc/PyLag.git",
+      packages=["pylag"],
+      ext_modules=cythonize(["pylag/*.pyx"], include_path = ["./include"]),
 )
