b28110b3aeb7c4616d8b1d3c1ebb95dc4bf4cb62
James <jcl@pml.ac.uk>
2015-12-02 08:50:11 +0000

    Ensure the ordering of interpolation coefficients matches the ordering of nbe.

--- pylag/unstruct_grid_tools.py
+++ pylag/unstruct_grid_tools.py
@@ -8,21 +8,28 @@ def create_fvcom_grid_metrics_file(ncin_file_name):
     ncout_file_name = 'grid_metrics.nc'
 
     # Make new file with global attributes and copied variables
-    var_to_copy = ['nv', 'nbe', 'x', 'y', 'xc', 'yc', 'siglev', 'siglay', 'h', 'a1u', 'a2u', 'aw0', 'awx', 'awy']
+    var_to_copy = ['nv', 'nbe', 'x', 'y', 'xc', 'yc', 'siglev', 'siglay', 'h', 'a1u', 'a2u']
     os.system("ncks -O -v "+",".join(var_to_copy)+" "+ncin_file_name+" "+ncout_file_name)
     
-    # Add nv and nbe
+    # Update nv, nbe, a1u and a2u
     ds_in = Dataset(ncin_file_name, 'r')
     nv = ds_in.variables['nv'][:] - 1 # -1 for zero based numbering
     nbe = ds_in.variables['nbe'][:] - 1 # -1 for zero based numbering
+    a1u = ds_in.variables['a1u'][:]
+    a2u = ds_in.variables['a2u'][:]
     
     # Sort the adjacency array
     nbe_sorted = sort_adjacency_array(nv, nbe)
+
+    # Sort interpolation coefficients to match nbe_sorted
+    a1u_sorted, a2u_sorted = sort_interpolants(a1u, a2u, nbe, nbe_sorted)
     
     # Write updates nv and nbe variables to file
     ds_out = Dataset(ncout_file_name,'a')
     ds_out.variables['nv'][:] = nv[:]
     ds_out.variables['nbe'][:] = nbe_sorted[:]
+    ds_out.variables['a1u'][:] = a1u_sorted[:]
+    ds_out.variables['a2u'][:] = a2u_sorted[:]
     
     # Close files
     ds_in.close()
@@ -108,7 +115,41 @@ def sort_adjacency_array(nv, nbe):
         nbe_sorted[2,i] = index_side3
 
     return nbe_sorted
+
+def sort_interpolants(a1u, a2u, nbe, nbe_sorted):
+    """
+    Match entries in axu to nbe_sorted, given that axu is currently matched to 
+    nbe.
+    """
+    
+    n_elems = a1u.shape[1]
+
+    if n_elems != a2u.shape[1] or n_elems != nbe.shape[1] or n_elems != nbe_sorted.shape[1]:
+        raise ValueError('Array dimensions do not match')
+
+    a1u_sorted = np.empty_like(a1u)
+    a2u_sorted = np.empty_like(a2u)
+
+    # Host element index is 0
+    a1u_sorted[0,:] = a1u[0,:]
+    a2u_sorted[0,:] = a2u[0,:]
+
+    for j in range(n_elems):
+        for i in xrange(3):
+            if nbe_sorted[i,j] == nbe[0,j]:
+                a1u_sorted[i+1,j] = a1u[1,j]
+                a2u_sorted[i+1,j] = a2u[1,j]
+            elif nbe_sorted[i,j] == nbe[1,j]:
+                a1u_sorted[i+1,j] = a1u[2,j]
+                a2u_sorted[i+1,j] = a2u[2,j]
+            elif nbe_sorted[i,j] == nbe[2,j]:
+                a1u_sorted[i+1,j] = a1u[3,j]
+                a2u_sorted[i+1,j] = a2u[3,j]
+            else:
+                raise ValueError('Failed to match entry in nbe and nbe_sorted.')
         
+    return a1u_sorted, a2u_sorted    
+
 def _get_empty_arrays():
     side1 = np.empty(2)
     side2 = np.empty(2)
