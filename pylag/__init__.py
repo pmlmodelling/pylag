a0eb4dbeb16755a625c73cedb254e41099e39833
James <jcl@pml.ac.uk>
2016-03-04 11:26:14 +0000

    Add main import to __init__.py plus some documentation.

---
+++ pylag/__init__.py
@@ -0,0 +1,16 @@
+"""
+PyLag
+-----
+
+A particle tracking tool for marine applications.
+
+Address:
+Plymouth Marine Laboratory
+Prospect Place, The Hoe
+Plymouth, PL1 3DH, UK
+
+Conact:
+James Clark (Plymouth Marine Laboratory). Email: jcl@pml.ac.uk
+"""
+
+from main import main
