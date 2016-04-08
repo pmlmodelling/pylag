3fe0cbd915331f32823361282391da93a83da871
James <jcl@pml.ac.uk>
2016-04-08 15:40:41 +0100

    Add matplotlib plot_directive for documentation plot generation.

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,8 @@ import sphinx_rtd_theme
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 
+              'sphinx.ext.viewcode', 'matplotlib.sphinxext.plot_directive']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
