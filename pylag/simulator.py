6a3dd7a760a68233c4adb5aafd67dd9566448bc1
James Clark <jcl@pml.ac.uk>
2016-09-14 23:21:20 +0100

    Implement ensemble main loop in serial simulator.

--- pylag/simulator.py
+++ pylag/simulator.py
@@ -28,7 +28,7 @@ class TraceSimulator(Simulator):
         
         # Model object
         self.model = get_model(config)
-        
+
         # Set up data access for the simulation start and end times
         self.model.read_data(self.time_manager.datetime_start,
                              self.time_manager.datetime_end)
@@ -54,33 +54,52 @@ class TraceSimulator(Simulator):
         # Initialise time counters, create particle seed
         self.model.create_seed(self.time_manager.time, group_ids, x_positions, \
             y_positions, z_positions)
-            
-        # Data logger
-        file_name = config.get('GENERAL', 'output_file')
-        start_datetime = config.get("SIMULATION", "start_datetime")
-        self.data_logger = NetCDFLogger(file_name, start_datetime, n_particles)
-        
-        # Write particle group ids to file
-        self.data_logger.write_group_ids(group_ids)
 
-        # Write initial state to file
-        particle_diagnostics = self.model.get_diagnostics(self.time_manager.time)
-        self.data_logger.write(self.time_manager.time, particle_diagnostics)
-
-        # The main update loop
-        print 'Starting PyLag\n'
-        print 'Progress:'
-        pbar = ProgressBar(maxval=self.time_manager.time_end, term_width=50).start()
-        while self.time_manager.time < self.time_manager.time_end:
-            self.model.update(self.time_manager.time)
-            self.time_manager.update_current_time()
-            if self.time_manager.write_output_to_file() == 1:
-                particle_diagnostics = self.model.get_diagnostics(self.time_manager.time)
-                self.data_logger.write(self.time_manager.time, particle_diagnostics)
+        # Run the ensemble
+        release = 0
+        while release < self.time_manager.number_of_particle_releases:
+            # Seed the model
+            self.model.seed()
             
-            # Check on status of reading frames and update if necessary
-            # Communicate updated arrays to the data reader if these are out of
-            # date.
-            self.model.update_reading_frames(self.time_manager.time)
-            pbar.update(self.time_manager.time)
-        pbar.finish()
+            # Create data logger
+            file_name = ''.join([config.get('GENERAL', 'output_file'), '_{}'.format(release)])
+            start_datetime = self.time_manager.datetime_start
+            self.data_logger = NetCDFLogger(file_name, start_datetime, n_particles)
+
+            # Write particle group ids to file
+            self.data_logger.write_group_ids(group_ids)
+
+            # Write initial state to file
+            particle_diagnostics = self.model.get_diagnostics(self.time_manager.time)
+            self.data_logger.write(self.time_manager.time, particle_diagnostics)
+
+            # The main update loop
+            print '\nStarting ensemble member {} ...'.format(release)
+            print 'Progress:'
+            pbar = ProgressBar(maxval=self.time_manager.time_end, term_width=50).start()
+            while self.time_manager.time < self.time_manager.time_end:
+                self.model.update(self.time_manager.time)
+                self.time_manager.update_current_time()
+                if self.time_manager.write_output_to_file() == 1:
+                    particle_diagnostics = self.model.get_diagnostics(self.time_manager.time)
+                    self.data_logger.write(self.time_manager.time, particle_diagnostics)
+
+                # Check on status of reading frames and update if necessary
+                # Communicate updated arrays to the data reader if these are out of
+                # date.
+                self.model.update_reading_frames(self.time_manager.time)
+                pbar.update(self.time_manager.time)
+            pbar.finish()
+            
+            # Close the current data logger
+            self.data_logger.close()
+            
+            # Update release counters
+            self.time_manager.update_release_counters()
+
+            # Set up data access for the new simulation
+            self.model.read_data(self.time_manager.datetime_start,
+                                 self.time_manager.datetime_end)
+
+            # Update release counter
+            release += 1
