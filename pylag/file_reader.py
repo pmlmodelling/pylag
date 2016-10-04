85a24cdd0fba5b738534375f5cffd6d25c56ee0c
James Clark <jcl@pml.ac.uk>
2016-10-04 16:42:23 +0100

    Ensure file_reader objects pass back ndarrays even when all dimensions have been indexed.

--- pylag/file_reader.py
+++ pylag/file_reader.py
@@ -60,10 +60,10 @@ class FileReader(object):
         return self._time[self._tidx_next]
 
     def get_time_dependent_variable_at_last_time_index(self, var_name):
-        return self._current_data_file.variables[var_name][self._tidx_last,:].squeeze()
+        return np.asarray(self._current_data_file.variables[var_name][self._tidx_last,:].squeeze())
     
     def get_time_dependent_variable_at_next_time_index(self, var_name):
-        return self._current_data_file.variables[var_name][self._tidx_next,:].squeeze()
+        return np.asarray(self._current_data_file.variables[var_name][self._tidx_next,:].squeeze())
 
     def _setup_file_access(self):
         logger = logging.getLogger(__name__)
