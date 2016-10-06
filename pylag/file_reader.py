c0b31d4080bffc598f965f3d50ce17cefc5cfe7b
James Clark <jcl@pml.ac.uk>
2016-10-06 12:34:34 +0100

    Ensure data read from file is returned as an array
    When all dimensions of an numpy array are indexed a floating point
    value is returned. As this is not what is expected by PyLag a
    new array object is created and returned instead. This should
    only be required when working with 1D models like GOTM.
    
    [ticket: x]

--- pylag/file_reader.py
+++ pylag/file_reader.py
@@ -60,10 +60,16 @@ class FileReader(object):
         return self._time[self._tidx_next]
 
     def get_time_dependent_variable_at_last_time_index(self, var_name):
-        return np.asarray(self._current_data_file.variables[var_name][self._tidx_last,:].squeeze())
+        var = self._current_data_file.variables[var_name][:].squeeze()
+        if var.ndim == 1:
+            return np.array([var[self._tidx_last]])
+        return var[self._tidx_last,:]
     
     def get_time_dependent_variable_at_next_time_index(self, var_name):
-        return np.asarray(self._current_data_file.variables[var_name][self._tidx_next,:].squeeze())
+        var = self._current_data_file.variables[var_name][:].squeeze()
+        if var.ndim == 1:
+            return np.array([var[self._tidx_next]])
+        return var[self._tidx_next,:]
 
     def _setup_file_access(self):
         logger = logging.getLogger(__name__)
