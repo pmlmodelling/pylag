645b4b330e9eb159491dfb95c1632c3e167ca316
James Clark <jcl@pml.ac.uk>
2016-11-01 16:46:09 +0000

    Fix shepard interpolation unit test.

--- tests/interpolation_test.py
+++ tests/interpolation_test.py
@@ -2,23 +2,28 @@ import numpy as np
 import numpy.testing as test
 
 #import pylag.interpolation as interp
+from pylag.data_types_python import DTYPE_FLOAT, DTYPE_INT
 from pylag import interpolation as interp
 from pylag import cwrappers
 
 def test_get_barycentric_coords():
     xpos = 1.0
     ypos = 1.0
-    x_tri = np.array([0.0, 2.0, 0.0], dtype=np.float32)
-    y_tri = np.array([0.0, 0.0, 2.0], dtype=np.float32)
+    x_tri = np.array([0.0, 2.0, 0.0], dtype=DTYPE_FLOAT)
+    y_tri = np.array([0.0, 0.0, 2.0], dtype=DTYPE_FLOAT)
     phi = cwrappers.get_barycentric_coords(xpos, ypos, x_tri, y_tri)
     test.assert_array_almost_equal(phi, [0.5, 0.5, 0.0])
 
 def test_shephard_interpolation():
-    xpts = np.array([0.0, 1.0], dtype=np.float32)
-    ypts = np.array([0.0, 1.0], dtype=np.float32)
-    vals = np.array([1.0, 2.0], dtype=np.float32)
-    val = interp.shephard_interpolation(0.5, 0.5, 2, xpts, ypts, vals)
-    test.assert_almost_equal(val, 1.5)
+    xpts = np.array([-2.0, -1.0, 1.0, 2.0], dtype=DTYPE_FLOAT)
+    ypts = np.array([-2.0, -1.0, 1.0, 2.0], dtype=DTYPE_FLOAT)
+    vals = np.array([0.0, 0.0, 1.0, 0.0], dtype=DTYPE_FLOAT)
+
+    val = cwrappers.shepard_interpolation(1.0, 1.0, xpts, ypts, vals)
+    test.assert_almost_equal(val, 1.0)
+    
+    val = cwrappers.shepard_interpolation(0.0, 0.0, xpts, ypts, vals)
+    test.assert_almost_equal(val, 0.4)
 
 def test_get_euclidian_distance():
     r1 = interp.get_euclidian_distance(0.0, 0.0, 3.0, 4.0)
