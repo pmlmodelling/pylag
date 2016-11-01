66db1497fe4d39f92c38c47eee77b13f813cc622
James Clark <jcl@pml.ac.uk>
2016-11-01 17:29:17 +0000

    Fix FVCOMDataReader unit tests

--- tests/fvcom_data_reader_test.py
+++ tests/fvcom_data_reader_test.py
@@ -1,22 +1,38 @@
 from unittest import TestCase
 import numpy.testing as test
+import datetime
 from ConfigParser import SafeConfigParser
 
 from pylag.fvcom_data_reader import FVCOMDataReader
 from pylag import cwrappers
-        
+
+from pylag.mediator import SerialMediator
+
 class FVCOMDataReader_test(TestCase):
 
     def setUp(self):
+        # Create config
         config = SafeConfigParser()
         config.add_section("SIMULATION")
         config.add_section("OCEAN_CIRCULATION_MODEL")
         config.set('SIMULATION', 'start_datetime', '2013-01-06 00:00:00')
+        config.set('SIMULATION', 'end_datetime', '2013-01-06 01:00:00')
         config.set('OCEAN_CIRCULATION_MODEL', 'data_dir', '../resources/')
-        config.set('OCEAN_CIRCULATION_MODEL', 'grid_metrics_file', 'fvcom_grid_metrics_test.nc')
+        config.set('OCEAN_CIRCULATION_MODEL', 'grid_metrics_file', '../resources/fvcom_grid_metrics_test.nc')
         config.set('OCEAN_CIRCULATION_MODEL', 'data_file_stem', 'fvcom_data_test')
-        config.set('OCEAN_CIRCULATION_MODEL', 'rounding_interval', '3600')
-        self.data_reader = FVCOMDataReader(config)
+        config.set('OCEAN_CIRCULATION_MODEL', 'rounding_interval', '3600')        
+        
+        # Create mediator
+        mediator = SerialMediator(config)
+        
+        # Create data reader
+        self.data_reader = FVCOMDataReader(config, mediator)
+        
+        # Read in data
+        datetime_start = datetime.datetime.strptime(config.get('SIMULATION', 'start_datetime'), "%Y-%m-%d %H:%M:%S")
+        datetime_end = datetime.datetime.strptime(config.get('SIMULATION', 'end_datetime'), "%Y-%m-%d %H:%M:%S")
+        self.data_reader.setup_data_access(datetime_start, datetime_end)
+        self.data_reader.read_data(0.0)
 
     def tearDown(self):
         del(self.data_reader)
@@ -58,81 +74,86 @@ class FVCOMDataReader_test(TestCase):
         xpos = 365751.7
         ypos = 5323568.0
         host = 0
+        zlayer = 0
 
         zpos = 0.0
         time = 0.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [2.0, 2.0, 0.0])
 
         zpos = 0.0
         time = 1800.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [3.0, 3.0, 0.0])
         
         zpos = -0.1
         time = 0.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [2.0, 2.0, 0.041666667])
 
     def test_get_velocity_in_middle_layer(self):
         xpos = 365751.7
         ypos = 5323568.0
         host = 0
+        zlayer = 1
 
         zpos = -0.3
         time = 0.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [1.5, 1.5, 0.083333333])
 
         zpos = -0.3
         time = 1800.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [2.25, 2.25, 0.12])
 
     def test_get_velocity_in_bottom_layer(self):
         xpos = 365751.7
         ypos = 5323568.0
         host = 0
+        zlayer = 2
 
         zpos = -1.0
         time = 0.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [0.0, 0.0, 0.0])
 
         zpos = -1.0
         time = 1800.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [0.0, 0.0, 0.0])
         
         zpos = -0.9
         time = 0.0
-        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host)
+        vel = cwrappers.get_velocity(self.data_reader, time, xpos, ypos, zpos, host, zlayer)
         test.assert_array_almost_equal(vel, [0.0, 0.0, 0.041666667])
 
     def test_get_vertical_eddy_diffusivity(self):
         xpos = 365751.7
         ypos = 5323568.0
         host = 0
+        zlayer = 0
 
         zpos = -0.1
         time = 0.0
 
-        diffusivity = self.data_reader.get_vertical_eddy_diffusivity(time, xpos, ypos, zpos, host)
+        diffusivity = self.data_reader.get_vertical_eddy_diffusivity(time, xpos, ypos, zpos, host, zlayer)
         
-        # Should be 0.5*(0.0 + 0.01)/12 = 0.00041666666 at a position half 
+        # Should be 0.5*(0.0 + 0.01)/144 = 0.000034722222 at a position half 
         # way between the top two sigma levels (-0.1) at t=0.0s, with a water 
         # depth of (11.0 + 1.0)m. Units are s^{-1}.
-        test.assert_almost_equal(diffusivity, 0.0004166666667)
+        test.assert_almost_equal(diffusivity, 0.000034722222 )
 
     def test_get_vertical_eddy_diffusivity_derivative(self):
         xpos = 365751.7
         ypos = 5323568.0
         host = 0
+        zlayer = 0
 
         zpos = -0.1
         time = 0.0
 
-        diffusivity_gradient = self.data_reader.get_vertical_eddy_diffusivity_derivative(time, xpos, ypos, zpos, host)
+        diffusivity_gradient = self.data_reader.get_vertical_eddy_diffusivity_derivative(time, xpos, ypos, zpos, host, zlayer)
         
         # Should be (0.0 - 0.01)/(0.0 - (-0.2))/(12*12) = -0.000347222 at a position 
         # half way between the top two sigma levels (-0.1) at t=0.0s, with a 
