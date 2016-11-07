561f5f65d420a3d53c1e40a63b4ce3949bb130aa
James Clark <jcl@pml.ac.uk>
2016-11-07 18:15:26 +0000

    Add class to help test integration schemes
    As Particle and Delta objects are now C structs, it became necessary
    to implement a new wrapper class to help with testing PyLag's
    numerical integration schemes.

--- pylag/cwrappers.pyx
+++ pylag/cwrappers.pyx
@@ -1,7 +1,7 @@
 """
-For speed, several of PyLag's cython functions take carrays as arguments. Cython
-does not appear to produce python wrappers for these functions, so here we
-manually implement a set of wrappers that do this.
+For speed, several of PyLag's Cython functions take c arrays or pointers as 
+arguments. Cython does not appear to produce python wrappers for these 
+functions, so here we manually implement a set of wrappers that do this.
 """
 
 include "constants.pxi"
@@ -9,17 +9,21 @@ include "constants.pxi"
 import numpy as np
 
 from pylag.data_types_cython cimport DTYPE_INT_t, DTYPE_FLOAT_t
+from integrator import get_num_integrator
 
 # PyLag cimports
 cimport pylag.interpolation as interp
 from data_reader cimport DataReader
+from particle cimport Particle
+from delta cimport Delta, reset
+from integrator cimport NumIntegrator
 
 cpdef get_barycentric_coords(x, y, x_tri, y_tri):
     cdef DTYPE_FLOAT_t x_tri_c[N_VERTICES]
     cdef DTYPE_FLOAT_t y_tri_c[N_VERTICES]
     cdef DTYPE_FLOAT_t phi_c[N_VERTICES]
     cdef DTYPE_INT_t i
-    
+
     if x_tri.shape[0] != N_VERTICES or y_tri.shape[0] != N_VERTICES:
         raise ValueError('1D array must be have a length of {}.'.format(N_VERTICES))
     
@@ -85,3 +89,46 @@ cpdef get_horizontal_velocity(DataReader data_reader, t, x, y, z, host, zlayer):
     for i in xrange(2):
         vel_out[i] = vel_c[i]
     return vel_out
+
+cdef class TestRK4Integrator:
+    """ Test class for Fourth Order Runga Kutta numerical integration schemes
+    
+    Parameters:
+    -----------
+    config : SafeConfigParser
+        Configuration object.
+    """
+    cdef NumIntegrator _num_integrator
+    
+    def __init__(self, config):
+        
+        self._num_integrator = get_num_integrator(config)
+    
+    def advect(self, data_reader, time, xpos, ypos, zpos):
+        cdef Particle particle
+        cdef Delta delta_X
+
+        # Set these properties to default values
+        particle.group_id = 0
+        particle.host_horizontal_elem = 0
+        particle.host_z_layer = 0
+        particle.in_domain = True
+
+        # Initialise remaining particle properties using the supplied arguments
+        particle.xpos = xpos
+        particle.ypos = ypos
+        particle.zpos = zpos
+        
+        # Reset Delta object
+        reset(&delta_X)
+        
+        # Advect the particle
+        self._num_integrator.advect(time, &particle, data_reader, &delta_X)
+        
+        # Used Delta values to update the particle's position
+        xpos_new = particle.xpos + delta_X.x
+        ypos_new = particle.ypos + delta_X.y
+        zpos_new = particle.zpos + delta_X.z
+
+        # Return the updated position
+        return xpos_new, ypos_new, zpos_new
