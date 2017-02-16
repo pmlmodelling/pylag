4dfed7be7a01a6902e07f8a93fe876ca17e02ea2
James Clark <jcl@pml.ac.uk>
2017-02-16 11:03:47 +0000

    Adapt FVCOM find_zlayer to work with cartesian z

--- pylag/fvcom_data_reader.pyx
+++ pylag/fvcom_data_reader.pyx
@@ -616,31 +616,39 @@ cdef class FVCOMDataReader(DataReader):
     cpdef DTYPE_INT_t find_zlayer(self, DTYPE_FLOAT_t time, DTYPE_FLOAT_t xpos,
         DTYPE_FLOAT_t ypos, DTYPE_FLOAT_t zpos, DTYPE_INT_t host,
         DTYPE_INT_t guess):
-        """ Find the host vertical layer
+        """ Find the host depth layer
         
-        Find the host sigma layer. Sigma levels are counted up from 0
-        starting at the surface and moving downwards. The current sigma layer is
+        Find the depth layer containing zpos. In FVCOM, Sigma levels are counted
+        up from 0 starting at the surface, where sigma = 0, and moving downwards
+        to the sea floor where sigma = -1. The current sigma layer is
         found by determining the two sigma levels that bound the given z
         position. Here, `guess' is ignored.
         """
         cdef DTYPE_FLOAT_t phi[N_VERTICES]
 
-        cdef DTYPE_FLOAT_t sigma_upper_level, sigma_lower_level
+        cdef DTYPE_FLOAT_t sigma, sigma_upper_level, sigma_lower_level
+
+        cdef DTYPE_FLOAT_t h, zeta
 
         cdef DTYPE_INT_t k
 
         # Compute barycentric coordinates for the given x/y coordinates
         self._get_phi(xpos, ypos, host, phi)
 
+        # Compute sigma
+        h = self.get_bathymetry(xpos, ypos, host)
+        zeta = self.get_bathymetry(time, xpos, ypos, host)
+        sigma = cartesian_to_sigma_coords(zpos, h, zeta)
+
         # Loop over all levels to find the host z layer
         for k in xrange(self._n_siglay):
             sigma_upper_level = self._interp_on_sigma_level(phi, host, k)
             sigma_lower_level = self._interp_on_sigma_level(phi, host, k+1)
             
-            if zpos <= sigma_upper_level and zpos >= sigma_lower_level:
+            if sigma <= sigma_upper_level and sigma >= sigma_lower_level:
                 return k
         
-        raise ValueError("Particle zpos (={}) not found!".format(zpos))
+        raise ValueError("Particle zpos (={}) not found! h = {}, zeta = {}.".format(zpos, h, zeta))
 
     cpdef DTYPE_FLOAT_t get_zmin(self, DTYPE_FLOAT_t time, DTYPE_FLOAT_t xpos,
             DTYPE_FLOAT_t ypos, DTYPE_INT_t host):
