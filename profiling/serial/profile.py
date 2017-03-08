6340e1225bec3a789a666f571fb8a60c7fa295ad
James Clark <jcl@pml.ac.uk>
2017-03-08 16:43:29 +0000

    Add calls to profiling stats

--- profiling/serial/profile.py
+++ profiling/serial/profile.py
@@ -8,4 +8,5 @@ subprocess.call(["python", "-m", "cProfile", "-o", profile_out, "prof_main.py", 
 s = pstats.Stats(profile_out)
 s.strip_dirs().sort_stats("time").print_stats(20)
 s.strip_dirs().sort_stats('cumulative').print_stats(20)
+s.strip_dirs().sort_stats("calls").print_stats(20)
 
