f329f33e49f84a79ecdd0bc16b47121b33800624
James Clark <jcl@pml.ac.uk>
2017-08-03 10:59:14 +0100

    Fix test config parameters

--- tests/numerics_test.py
+++ tests/numerics_test.py
@@ -72,7 +72,7 @@ def test_set_valid_OS1NumMethod_advection_and_diffusion_time_steps():
     config.set('SIMULATION', 'vert_bound_cond', 'None')
 
     config.add_section("NUMERICS")
-    config.set('NUMERICS', 'num_method', 'operator_split_0')
+    config.set('NUMERICS', 'num_method', 'operator_split_1')
     config.set('NUMERICS', 'adv_iterative_method', 'Adv_RK4_3D')
     config.set('NUMERICS', 'diff_iterative_method', 'Diff_Milstein_3D')
 
@@ -92,7 +92,7 @@ def test_set_invalid_OS1NumMethod_diffusion_time_step_that_is_not_equal_to_half_
     config.set('SIMULATION', 'vert_bound_cond', 'None')
 
     config.add_section("NUMERICS")
-    config.set('NUMERICS', 'num_method', 'operator_split_0')
+    config.set('NUMERICS', 'num_method', 'operator_split_1')
     config.set('NUMERICS', 'adv_iterative_method', 'Adv_RK4_3D')
     config.set('NUMERICS', 'diff_iterative_method', 'Diff_Milstein_3D')
 
