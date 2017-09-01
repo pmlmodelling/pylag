f24388496faa0a375685eaca7fc122a94b85aaae
James Clark <jcl@pml.ac.uk>
2017-09-01 15:46:13 +0100

    Add open boundary flags to FVCOM grid metrics files

--- pylag/utils.py
+++ pylag/utils.py
@@ -29,11 +29,14 @@ def round_time(datetime_raw, rounding_interval=3600):
         datetime_rounded.append(dt + datetime.timedelta(0,rounding-seconds,-dt.microsecond))
     return datetime_rounded
 
-def create_fvcom_grid_metrics_file(ncin_file_name, ncout_file_name):
+def create_fvcom_grid_metrics_file(ncin_file_name, obc_file_name, 
+                                   ncout_file_name='./grid_metrics.nc'):
     """Create FVCOM grid metrics file
     
-    Grid variables saved by FVCOM are not necessarily ordered in a way that 
-    can be understood by PyLag. This function rectifies this by generating a 
+    In FVCOM output files, the grid variables nv and nbe are not ordered in the
+    way PyLag expects them to be. Nor do FVCOM output files distinguish between
+    open and land boundary nodes - all sides lying along a boundary are simply
+    given a -1 flag. This function rectifies these problems by generating a 
     separate grid metrics file that can be passed to PyLag.
     
     NB This function only needs to be called once per FVCOM model grid - the 
@@ -41,23 +44,25 @@ def create_fvcom_grid_metrics_file(ncin_file_name, ncout_file_name):
     
     Parameters:
     -----------
-    nv : 2D ndarray, int
-        Nodes surrounding element, shape (3, n_elems)
-        
-    nbe : 2D ndarray, int
-        Elements surrounding element, shape (3, n_elems)
-        
-    Returns:
-    --------
-    nbe_sorted: 2D ndarray, int
-        The new nbe array
+    ncin_file_name : str
+        The path to an FVCOM output file that can be read in a processed
+    obc_file_name : str
+        The path to the text file containing a list of open boundary nodes
+    ncout_file_name : str, optional
+        The name of the grid metrics file that will be created        
     """
+    print "Creating FVCOM grid metrics file {}".format(ncout_file_name)
+
+    ob_nodes = get_open_boundary_nodes(obc_file_name)
+    
     # Make new file with global attributes and copied variables
     var_to_copy = ['nv', 'nbe', 'x', 'y', 'xc', 'yc', 'lat', 'lon', 'latc', 
             'lonc', 'siglev', 'siglay', 'h', 'a1u', 'a2u']
     os.system("ncks -O -v "+",".join(var_to_copy)+" "+ncin_file_name+" "+ncout_file_name)
     
     # Update nv, nbe, a1u and a2u
+    # ---------------------------
+    
     ds_in = Dataset(ncin_file_name, 'r')
     nv = ds_in.variables['nv'][:] - 1 # -1 for zero based numbering
     nbe = ds_in.variables['nbe'][:] - 1 # -1 for zero based numbering
@@ -69,11 +74,14 @@ def create_fvcom_grid_metrics_file(ncin_file_name, ncout_file_name):
 
     # Sort interpolation coefficients to match nbe_sorted
     a1u_sorted, a2u_sorted = sort_interpolants(a1u, a2u, nbe, nbe_sorted)
-    
-    # Write updates nv and nbe variables to file
+
+    # Flag open boundaries
+    nbe_sorted_with_ob_flags = add_open_boundary_flags(nv, nbe_sorted, ob_nodes)
+
+    # Write updates to the nv, nbe, a1u and a2u variables to file
     ds_out = Dataset(ncout_file_name,'a')
     ds_out.variables['nv'][:] = nv[:]
-    ds_out.variables['nbe'][:] = nbe_sorted[:]
+    ds_out.variables['nbe'][:] = nbe_sorted_with_ob_flags[:]
     ds_out.variables['a1u'][:] = a1u_sorted[:]
     ds_out.variables['a2u'][:] = a2u_sorted[:]
     
@@ -190,6 +198,66 @@ def sort_interpolants(a1u, a2u, nbe, nbe_sorted):
                 raise ValueError('Failed to match entry in nbe and nbe_sorted.')
         
     return a1u_sorted, a2u_sorted    
+
+def get_open_boundary_nodes(file_name):
+    """Read open boundary nodes from file
+    
+    Parameters:
+    -----------
+    file_name : str
+        Name of file containing a list of the open boundary nodes
+    """
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    
+    # Number of open boundary nodes given on first line
+    n_obc_nodes = int(lines.pop(0).strip().split(' ')[-1])
+    print "Grid has {} nodes on the open boundary".format(n_obc_nodes)
+    
+    nodes = []
+    for line in lines:
+        nodes.append(int(line.strip().split(' ')[1]))
+    
+    if n_obc_nodes != len(nodes):
+        raise RuntimeError('Error reading open boundary node list file.')
+    
+    return nodes
+
+def add_open_boundary_flags(nv, nbe, ob_nodes):
+    """Add open boundary flags
+    
+    For each element, the method checks to see if two of the element's nodes lie
+    on the open boundary. If they do, it flags the corresponding neighbour
+    element with a -2, rather than a -1 as is the case in FVCOM output files.
+    
+    Parameters:
+    -----------
+    nv : 2D ndarray, int
+        Nodes surrounding element, shape (3, n_elems)
+        
+    nbe : 2D ndarray, int
+        Elements surrounding element, shape (3, n_elems)
+    
+    ob_nodes : list, int
+        List of nodes that lie along the open boundary
+    """
+    n_elems = nv.shape[1]
+    
+    nbe_new = nbe.copy()
+    for i in range(n_elems):
+        nodes = set(nv[:,i]).intersection(ob_nodes)
+        if len(nodes) == 2:
+            
+            # Element borders the open boundary
+            if len(nodes.intersection([nv[1,i],nv[2,i]])) == 2:
+                nbe_new[0, i] = -2
+            elif len(nodes.intersection([nv[2,i],nv[0,i]])) == 2:
+                nbe_new[1, i] = -2
+            elif len(nodes.intersection([nv[0,i],nv[1,i]])) == 2:
+                nbe_new[2, i] = -2
+            else:
+                raise RuntimeError('Failed to identify open boundary.')
+    return nbe_new
 
 def _get_empty_arrays():
     side1 = np.empty(2)
