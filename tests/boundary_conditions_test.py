5a8fd9850dc63c187b4f6cea2c898e03db2de1f6
James Clark <jcl@pml.ac.uk>
2018-08-22 17:20:58 +0100

    Update boundary conditions unit tests

--- tests/boundary_conditions_test.py
+++ tests/boundary_conditions_test.py
@@ -1,23 +1,30 @@
 from unittest import TestCase
 import numpy.testing as test
 
-from pylag.data_reader import DataReader
+# Test data readers
+from pylag.tests.boundary_conditions import TestHorizBCDataReader
+
+# Test calculators
 from pylag.boundary_conditions import RefHorizBoundaryConditionCalculator
 from pylag.boundary_conditions import RefVertBoundaryConditionCalculator
 
-class RefHorizBoundaryConditionCalculator_test(TestCase):
-    class TestDataReader(DataReader):
-        def get_boundary_intersection(self, xpos_old, ypos_old, xpos_new, ypos_new, elem):
-            x1 = -1.0; y1 = -1.0; x2 = 1.0; y2 = 1.0; xi=0.0; yi=0.0
-            return x1, y1, x2, y2, xi, yi
+# Test particles
+from pylag.particle import ParticleSmartPtr
 
-    def test_apply_reflecting_boundary_condition(self):
-        data_reader = RefHorizBoundaryConditionCalculator_test.TestDataReader()
-        horiz_bc_calculator = RefHorizBoundaryConditionCalculator()
-        x3 = 0.0; y3 = -1.0; x4 = 0.0; y4 = 1.0; elem = 0
-        xpos, ypos = horiz_bc_calculator.apply(data_reader, x3, y3, x4, y4, elem)
-        test.assert_almost_equal(xpos, 1.0)
-        test.assert_almost_equal(ypos, 0.0)
+
+def test_apply_reflecting_horizontal_boundary_condition():
+    
+    data_reader = TestHorizBCDataReader()
+    
+    horiz_bc_calculator = RefHorizBoundaryConditionCalculator()
+    
+    particle_old = ParticleSmartPtr(xpos=0.0, ypos=-1.0, host=0)
+    particle_new = ParticleSmartPtr(xpos=0.0, ypos=1.0, host=0)
+    
+    horiz_bc_calculator.apply_wrapper(data_reader, particle_old, particle_new)
+
+    test.assert_almost_equal(particle_new.xpos, 1.0)
+    test.assert_almost_equal(particle_new.ypos, 0.0)
 
 class RefVertBoundaryConditionCalculator_test(TestCase):
 
