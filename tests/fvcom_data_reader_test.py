04fd35acddafa01dd97f3bf185f9065815e2030e
James Clark <jcl@pml.ac.uk>
2018-08-25 10:45:44 +0100

    Simplify FVCOM test grid

--- tests/fvcom_data_reader_test.py
+++ tests/fvcom_data_reader_test.py
@@ -19,34 +19,26 @@ class MockFVCOMMediator(Mediator):
     """
     def __init__(self):
         # Dimension variables
-        n_elems = 4
-        n_nodes = 6
+        n_elems = 5
+        n_nodes = 7
         n_siglev = 4
         n_siglay = 3
         self._dim_vars = {'nele': n_elems, 'node': n_nodes,
                           'siglev': n_siglev, 'siglay': n_siglay}
                           
-        # Grid variables - copied from a real FVCOM output file
-        nv = np.array([[4,0,2,4],[3,1,4,5],[1,3,1,3]],dtype=int)
-        nbe = np.array([[1,0,0,-1],[2,-1,-1,0],[3,-1,-1,-1]],dtype=int)
-        x = np.array([370395.625, 367130.84375, 363094.219, 370100., 367030., 367652.656], dtype=float)
-        y = np.array([5321986.5, 5322253., 5322582.5, 5325070., 5325870., 5328112.5], dtype=float)
-        xc = np.array([368086.9, 369208.8, 365751.7, 368260.9], dtype=float)
-        yc = np.array([5324398.5, 5323103., 5323568.5, 5326351.], dtype=float)
+        # Grid variables
+        nv = np.array([[4,0,2,4,3],[3,1,4,5,6],[1,3,1,3,0]],dtype=int)
+        nbe = np.array([[1,0,0,-1,-1],[2,4,-1,0,1],[3,-1,-1,-1,-1]],dtype=int)
+        x = np.array([2.0, 1.0, 0.0, 2.0, 1.0, 1.5, 3.0], dtype=float)
+        y = np.array([1.0, 1.0, 1.0, 2.0, 2.0, 3.0, 1.0], dtype=float)
+        xc = np.array([1.3333333333, 1.6666666667, 0.6666666667, 1.5000000000, 2.3333333333], dtype=float)
+        yc = np.array([1.6666666667, 1.3333333333, 1.3333333333, 2.3333333333, 1.3333333333], dtype=float)
         siglay = np.array([[-.1,-.5,-.9],]*n_nodes, dtype=float).transpose()
         siglev = np.array([[0., -.2,-.8,-1.],]*n_nodes, dtype=float).transpose()
         h = np.array([10., 11., 10., 11., 11., 10.], dtype=float)
-        a1u = np.array([[0.0001532937, 0, 0, 0.0001793663],
-                        [0.0001951739, 0, 0, 0.0001998931],
-                        [-0.0003354263, 0, 0, -1.715655e-05],
-                        [-1.304125e-05, 0, 0, -0.0003621029]], dtype=float)
-        a2u = np.array([[7.157444e-06, 0, 0, 0.0001338511],
-                        [-0.0002355544, 0, 0, 0.0001577292],
-                        [-8.94851e-05, 0, 0, -0.0004162626],
-                        [0.0003178821, 0, 0, 0.0001246824]], dtype=float)
         self._grid_vars = {'nv': nv, 'nbe': nbe, 'x': x, 'y': y, 'xc': xc,
                            'yc': yc, 'siglay': siglay, 'siglev': siglev,
-                           'h': h, 'a1u': a1u, 'a2u': a2u}
+                           'h': h}
         
         # Dictionaries holding the value of time dependent and time independent variables
         zeta = np.array([[0.,1.,0.,1.,1.,0.],[0.,2.,0.,2.,2.,0.]],dtype=float)
@@ -133,72 +125,76 @@ class FVCOMDataReader_test(TestCase):
         del(self.data_reader)
 
     def test_find_host_using_global_search(self):
-        xpos = 368086.9375 # Centroid of element 0 (x coordinate)
-        ypos = 5324397.5 # Centroid of element 0 (y coordinate)
+        xpos = 1.3333333333 # Centroid of element 0 (x coordinate)
+        ypos = 1.6666666667 # Centroid of element 0 (y coordinate)
         host = self.data_reader.find_host_using_global_search(xpos, ypos)
         test.assert_equal(host, 0)
 
     def test_find_host_using_global_search_when_a_particle_is_in_an_element_with_two_land_boundaries(self):
-        xpos = 369208.8125 # Centroid of element 1 that has two land boundaries (x coordinate)
-        ypos = 5323103.0 # Centroid of element 1 that has two land boundaries (y coordinate)
+        xpos = 0.6666666667 # Centroid of element 1 that has two land boundaries (x coordinate)
+        ypos = 1.3333333333 # Centroid of element 1 that has two land boundaries (y coordinate)
         host = self.data_reader.find_host_using_global_search(xpos, ypos)
         test.assert_equal(host, -1)
 
     def test_find_host_when_particle_is_in_the_domain(self):
-        particle_old = ParticleSmartPtr(xpos=368260.875, ypos=5326351.0, host=3)
-        particle_new = ParticleSmartPtr(xpos=368086.9375, ypos=5324397.5)
+        particle_old = ParticleSmartPtr(xpos=1.5, ypos=2.3333333333, host=3)
+        particle_new = ParticleSmartPtr(xpos=1.3333333333, ypos=1.6666666667)
         flag = self.data_reader.find_host_wrapper(particle_old, particle_new)
         test.assert_equal(flag, 0)
         test.assert_equal(particle_new.host_horizontal_elem, 0)
 
     def test_find_host_when_particle_has_crossed_into_an_element_with_two_land_boundaries(self):
-        particle_old = ParticleSmartPtr(xpos=368086.9375, ypos=5324397.5, host=0)
-        particle_new = ParticleSmartPtr(xpos=369208.8125, ypos=5323103.0)
+        particle_old = ParticleSmartPtr(xpos=1.3333333333, ypos=1.6666666667, host=0)
+        particle_new = ParticleSmartPtr(xpos=0.6666666667, ypos=1.3333333333)
         flag = self.data_reader.find_host_wrapper(particle_old, particle_new)
         test.assert_equal(flag, -1)
         test.assert_equal(particle_new.host_horizontal_elem, 0)
 
     def test_find_host_when_particle_has_crossed_a_land_boundary(self):
-        particle_old = ParticleSmartPtr(xpos=369208.8125, ypos=5323103.0, host=1)
-        particle_new = ParticleSmartPtr(xpos=370267.0, ypos=5324350.0)
+        particle_old = ParticleSmartPtr(xpos=1.6666666667, ypos=1.3333333333, host=1)
+        particle_new = ParticleSmartPtr(xpos=1.5, ypos=0.0)
         flag = self.data_reader.find_host_wrapper(particle_old, particle_new)
         test.assert_equal(flag, -1)
         test.assert_equal(particle_new.host_horizontal_elem, 1)
 
     def test_find_host_when_particle_has_crossed_multiple_elements_to_an_element_with_two_land_boundaries(self):
-        particle_old = ParticleSmartPtr(xpos=369208.8125, ypos=5323103.0, host=1)
-        particle_new = ParticleSmartPtr(xpos=365751.6875, ypos=5323568.5)
+        particle_old = ParticleSmartPtr(xpos=1.6666666667, ypos=1.3333333333, host=1)
+        particle_new = ParticleSmartPtr(xpos=0.6666666667, ypos=1.3333333333)
         flag = self.data_reader.find_host_wrapper(particle_old, particle_new)
         test.assert_equal(flag, -1)
         test.assert_equal(particle_new.host_horizontal_elem, 0)
 
     def test_get_boundary_intersection_x2x0(self):
-        xpos_old = 369208.8125 # Centroid of element 1 (x coordinate)
-        ypos_old = 5323103.0 # Centroid of element 1 (y coordinate)
-        xpos_new = 370267.0 # Point outside element (x coordinate)
-        ypos_new = 5324350.0 # Point outside element (y coordinate)
+        xpos_old = 1.6666666667 # Centroid of element 1 (x coordinate)
+        ypos_old = 1.3333333333 # Centroid of element 1 (y coordinate)
+        xpos_new = 1.6666666667 # Point outside element (x coordinate)
+        ypos_new = 0.9 # Point outside element (y coordinate)
         last_host = 1
         x1, y1, x2, y2, xi, yi = self.data_reader.get_boundary_intersection(xpos_old, ypos_old, xpos_new, ypos_new, last_host)
-        test.assert_almost_equal(x1, 370100.0)
-        test.assert_almost_equal(y1, 5325070.0)
-        test.assert_almost_equal(x2, 370395.625)
-        test.assert_almost_equal(y2, 5321986.5)
+        test.assert_almost_equal(x1, 2.0)
+        test.assert_almost_equal(y1, 1.0)
+        test.assert_almost_equal(x2, 1.0)
+        test.assert_almost_equal(y2, 1.0)
+        test.assert_almost_equal(xi, 1.6666666667)
+        test.assert_almost_equal(yi, 1.0)
 
     def test_get_boundary_intersection_x0x1(self):
-        xpos_old = 369208.8125 # Centroid of element 1 (x coordinate)
-        ypos_old = 5323103.0 # Centroid of element 1 (y coordinate)
-        xpos_new = 368802.0 # Point outside element (x coordinate)
-        ypos_new = 5321920.0 # Point outside element (y coordinate)
+        xpos_old = 1.3333333333 # Centroid of element 0 (y coordinate)
+        ypos_old = 1.6666666667 # Centroid of element 0 (x coordinate)
+        xpos_new = 1.3333333333 # Point outside element (x coordinate)
+        ypos_new = 0.9 # Point outside element (y coordinate)
         last_host = 1
         x1, y1, x2, y2, xi, yi = self.data_reader.get_boundary_intersection(xpos_old, ypos_old, xpos_new, ypos_new, last_host)
-        test.assert_almost_equal(x1, 370395.625)
-        test.assert_almost_equal(y1, 5321986.5)
-        test.assert_almost_equal(x2, 367130.84375)
-        test.assert_almost_equal(y2, 5322253.0)
+        test.assert_almost_equal(x1, 2.0)
+        test.assert_almost_equal(y1, 1.0)
+        test.assert_almost_equal(x2, 1.0)
+        test.assert_almost_equal(y2, 1.0)
+        test.assert_almost_equal(xi, 1.3333333333)
+        test.assert_almost_equal(yi, 1.0)
 
     def test_get_zmin(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         time = 0.0
@@ -209,8 +205,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_almost_equal(bathy, -11.0)
 
     def test_get_zmax(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
         
         time = 0.0
@@ -227,8 +223,8 @@ class FVCOMDataReader_test(TestCase):
 
     def test_set_vertical_grid_vars_for_a_particle_on_the_sea_surface(self):
         time = 0.0
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         zpos = 1.0
         host = 0
 
@@ -242,8 +238,8 @@ class FVCOMDataReader_test(TestCase):
 
     def test_set_vertical_grid_vars_for_a_particle_on_the_sea_floor(self):
         time = 0.0
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         zpos = -11.0
         host = 0
 
@@ -258,8 +254,8 @@ class FVCOMDataReader_test(TestCase):
 
     def test_set_vertical_grid_vars_for_a_particle_in_the_surface_boundary_layer(self):
         time = 0.0
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         zpos = 0.4 # this is 25% of the way between the top and bottom sigma levels
         host = 0
 
@@ -273,8 +269,8 @@ class FVCOMDataReader_test(TestCase):
 
     def test_set_vertical_grid_vars_for_a_particle_in_the_bottom_boundary_layer(self):
         time = 0.0
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         zpos = -10.4
         host = 0
 
@@ -288,8 +284,8 @@ class FVCOMDataReader_test(TestCase):
 
     def test_set_vertical_grid_vars_for_a_particle_in_the_middle_of_the_water_column(self):
         time = 0.0
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         zpos = -2.6
         host = 0
 
@@ -305,8 +301,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_almost_equal(particle.omega_layers, 0.5)
 
     def test_get_velocity_in_surface_layer(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         # Test #1
@@ -345,8 +341,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_array_almost_equal(vel, [2.0, 2.0, 2.0])
 
     def test_get_velocity_in_middle_layer(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         # Test #1
@@ -372,8 +368,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_array_almost_equal(vel, [2.25, 2.25, 2.25])
 
     def test_get_velocity_in_bottom_layer(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         # Test #1
@@ -410,8 +406,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_array_almost_equal(vel, [0.0, 0.0, 0.0])
 
     def test_get_vertical_eddy_diffusivity(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         zpos = -0.2
@@ -423,8 +419,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_almost_equal(diffusivity,  0.005)
 
     def test_get_vertical_eddy_diffusivity_derivative(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         zpos = -0.2
@@ -436,8 +432,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_almost_equal(diffusivity_gradient, -0.0026042)
 
     def test_get_horizontal_eddy_viscosity(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         zpos = -0.1
@@ -449,8 +445,8 @@ class FVCOMDataReader_test(TestCase):
         test.assert_almost_equal(viscosity,  0.01)
 
     def test_get_horizontal_eddy_viscosity_derivative(self):
-        xpos = 368086.9
-        ypos = 5324398.5
+        xpos = 1.3333333333
+        ypos = 1.6666666667
         host = 0
 
         zpos = -0.1
