a00103880786ba4655dd16a85274b09c49091d75
James Clark <jcl@pml.ac.uk>
2019-03-28 13:55:26 +0000

    Add library of common variable names/units

---
+++ pylag/variable_library.py
@@ -0,0 +1,46 @@
+""" Library of standard PyLag variables
+
+This is a hardcoded library of standard variable names and units
+which is intended to help with the consistent writing of data to
+file.
+"""
+from pylag.data_types_python import DTYPE_INT, DTYPE_FLOAT
+
+
+def get_data_type(var_name):
+    return _variable_data_types[var_name]
+
+
+def get_units(var_name):
+    return _variable_units[var_name]
+
+
+def get_long_name(var_name):
+    return _variable_long_names[var_name]
+
+
+# Dictionaries holding variable attributes
+_variable_data_types = {}
+_variable_units = {}
+_variable_long_names = {}
+
+
+# Particle group ID
+_variable_data_types['group_id'] = DTYPE_INT
+_variable_units['group_id'] = 'NA'
+_variable_long_names['group_id'] = 'Particle group ID'
+
+# Particle x-position
+_variable_data_types['xpos'] = DTYPE_FLOAT
+_variable_units['xpos'] = 'Meters (m)'
+_variable_long_names['xpos'] = 'Particle x-position'
+
+# Particle y-position
+_variable_data_types['ypos'] = DTYPE_FLOAT
+_variable_units['ypos'] = 'Meters (m)'
+_variable_long_names['ypos'] = 'Particle y-position'
+
+# Particle z-position
+_variable_data_types['zpos'] = DTYPE_FLOAT
+_variable_units['zpos'] = 'Meters (m)'
+_variable_long_names['zpos'] = 'Particle z-position'
