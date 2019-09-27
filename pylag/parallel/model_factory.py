2f450976a2e6c401c376770cfbafffa9aa17161f
James Clark <jcl@pml.ac.uk>
2019-09-27 10:02:15 +0100

    Add missing datetime arguments
    Start and end time arguments were not being passed to MPIMediator
    on initialisation.

--- pylag/parallel/model_factory.py
+++ pylag/parallel/model_factory.py
@@ -5,13 +5,13 @@ from pylag.gotm_data_reader import GOTMDataReader
 # Parallel imports
 from pylag.parallel.mediator import MPIMediator
 
-def get_model(config):
+def get_model(config, datetime_start, datetime_end):
     if config.get("OCEAN_CIRCULATION_MODEL", "name") == "FVCOM":
-        mediator = MPIMediator(config)
+        mediator = MPIMediator(config, datetime_start, datetime_end)
         data_reader = FVCOMDataReader(config, mediator)
         return OPTModel(config, data_reader)
     elif config.get("OCEAN_CIRCULATION_MODEL", "name") == "GOTM":
-        mediator = MPIMediator(config)
+        mediator = MPIMediator(config, datetime_start, datetime_end)
         data_reader = GOTMDataReader(config, mediator)
         return OPTModel(config, data_reader)
     else:
