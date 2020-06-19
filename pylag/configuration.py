aee0f4cdf70e314b414e6349e922af32ff825618
James Clark <jcl@pml.ac.uk>
2020-06-19 16:20:51 +0100

    Update docs in configuration.py

--- pylag/configuration.py
+++ pylag/configuration.py
@@ -1,9 +1,47 @@
+"""
+PyLag runtime configuration options are set in a dedicated run configuration
+file, which typically has a name like "pylag.cfg". The run configuration file
+is formatted for configparser, which is used here to read in the configuration
+file. A object of type SafeConfigParser is saved as a module level variable
+with a reference returned to the caller. This structure allows the same
+ConfigParser to be passed to all PyLag objects that use information within
+the PyLag configuration file during class initialisation.
+"""
+
 try:
     import configparser
 except ImportError:
     import ConfigParser as configparser
 
 def get_config(config_filename=None):
+    """ Get the run config
+
+    When called for the first time, the config file name must
+    be provided. It will then be opened and parsed using
+    `configparser`. The resulting `ConfigParser` is saved as
+    a private, module-level variable. Subsequent calls to
+    this function can be used to obtain a reference to the
+    ConfigParser.
+
+    Parameters
+    ----------
+    config_filename : str or None, optional
+        The name of the run configuration file including its full or
+        relative path. This is only optional if the `ConfigParser`
+        has been created during a previous call to `get_config`.
+
+    Returns
+    -------
+    _config : SafeConfigParser
+        The ConfigParser.
+
+    Raises
+    ------
+    RuntimeError
+        Tried to call `get_config` for the first time without a file
+        name.
+
+    """
     global _config
     if _config is None:
         # Create new configuration object
@@ -17,6 +55,7 @@ def get_config(config_filename=None):
         # In the future, there may be a scenario when a useful config could be
         # created from defaults alone.
         raise RuntimeError('No configuration file provided.')
-        
+
+
 # ConfigParser
 _config = None
