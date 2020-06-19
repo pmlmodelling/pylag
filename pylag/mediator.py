db94d35b4b4345bda838f0ea1783ab2d11598115
James Clark <jcl@pml.ac.uk>
2020-06-19 17:59:38 +0100

    Update Mediator docs

--- pylag/mediator.py
+++ pylag/mediator.py
@@ -1,10 +1,18 @@
+"""
+The module provides a set of classes to assist with communication between
+objects of type DataReader and objects of type FileReader. Primarily, the
+mediator module was introduced in order to abstract away the MPI interface,
+making it easier to PyLag either in serial or parallel. It also assists with
+testing.
+"""
+
 from pylag.data_types_python import DTYPE_INT
 from pylag.file_reader import FileReader
 from pylag.file_reader import DiskFileNameReader
 from pylag.file_reader import NetCDFDatasetReader
 
 
-class Mediator(object):
+class Mediator:
     """ Base class for objects of type Mediator.
 
     Mediators manage data transfers between FileReaders and other objects. Data
@@ -12,46 +20,205 @@ class Mediator(object):
     type DataReader. The decoupling between FileReader and DataReader objects
     was put in place in order to support parallel simulations where it is 
     preferable for just a single process to access the file system.
-    
     """
     def setup_data_access(self, start_datetime, end_datetime):
+        """Wrapper for FileReader's setup_data_access
+
+        Parameters
+        ----------
+        start_datetime : Datetime
+            Simulation start date/time.
+
+        end_datetime : Datetime
+            Simulation end date/time.
+        """
         raise NotImplementedError
 
     def update_reading_frames(self, time):
+        """ Wrapper for FileReader's update_reading_frames
+
+        Parameters
+        ----------
+        time : float
+            Time
+        """
         raise NotImplementedError
     
     def get_dimension_variable(self, var_name):
+        """ Wrapper for FileReader's get_dimension_variable
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the dimension variable.
+
+        Returns
+        -------
+         : int
+             The size of the dimensions variable.
+        """
         raise NotImplementedError
     
     def get_grid_variable(self, var_name, var_dims, var_type):
+        """ Wrapper for FileReader's get_grid_variable
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        Returns
+        -------
+         : NDArray
+             The the grid variable.
+        """
         raise NotImplementedError
 
     def get_time_at_last_time_index(self):
+        """ Wrapper for FileReader's get_time_at_last_time_index
+
+         Returns
+         -------
+          : float
+              The time at the last time index.
+         """
         raise NotImplementedError
 
     def get_time_at_next_time_index(self):
+        """ Wrapper for FileReader's get_time_at_next_time_index
+
+         Returns
+         -------
+          : float
+              The time at the last time index.
+         """
         raise NotImplementedError
 
     def get_variable_dimensions(self, var_name):
+        """ Wrapper for FileReader's get_variable_dimension
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        Returns
+        -------
+         : tuple(str)
+             The variable's dimensions
+        """
         raise NotImplementedError
 
     def get_variable_shape(self, var_name):
+        """ Wrapper for FileReader's get_variable_shape
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        Returns
+        -------
+         : tuple(int)
+             The variable's shape
+        """
         raise NotImplementedError
 
     def get_time_dependent_variable_at_last_time_index(self, var_name, var_dims, var_type):
+        """ Wrapper for FileReader's function of the same name
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        var_dims : tuple
+             Tuple of variable dimensions.
+
+        var_type : type
+            The variable type.
+
+        Returns
+        -------
+         : NDArray
+             The variable array
+
+        """
         raise NotImplementedError
 
     def get_time_dependent_variable_at_next_time_index(self, var_name, var_dims, var_type):
+        """ Wrapper for FileReader's function of the same name
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        var_dims : tuple
+             Tuple of variable dimensions.
+
+        var_type : type
+            The variable type.
+
+        Returns
+        -------
+         : NDArray
+             The variable array
+
+        """
         raise NotImplementedError
 
     def get_mask_at_last_time_index(self, var_name, var_dims):
+        """ Wrapper for FileReader's function of the same name
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        var_dims : tuple
+             Tuple of variable dimensions.
+
+        Returns
+        -------
+         : NDArray
+             The variable array
+        """
         raise NotImplementedError
 
     def get_mask_at_next_time_index(self, var_name, var_dims):
+        """ Wrapper for FileReader's function of the same name
+
+        Parameters
+        ----------
+        var_name : str
+            The name of the variable.
+
+        var_dims : tuple
+             Tuple of variable dimensions.
+
+        Returns
+        -------
+         : NDArray
+             The variable array
+        """
         raise NotImplementedError
 
 
 class SerialMediator(Mediator):
+    """ Serial mediator
+
+    Serial mediator used for serial runs.
+
+    Attributes
+    ----------
+    config : SafeConfigParser
+        Run configuration object
+
+    file_reader : pylag.FileReader
+        FileReader object.
+    """
+
     def __init__(self, config, datetime_start, datetime_end):
         self.config = config
 
@@ -96,3 +263,7 @@ class SerialMediator(Mediator):
 
     def get_mask_at_next_time_index(self, var_name, var_dims):
         return self.file_reader.get_mask_at_next_time_index(var_name).astype(DTYPE_INT)
+
+
+__all__ = ['Mediator',
+           'SerialMediator']
