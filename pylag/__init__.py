3ebe8481e2448d5b063dccf6430bcda7cc8b6a6b
James Clark <jcl@pml.ac.uk>
2021-02-06 15:47:13 +0000

    Add standard imports to __init__.py

--- pylag/__init__.py
+++ pylag/__init__.py
@@ -12,4 +12,16 @@ Plymouth, PL1 3DH, UK
 Conact:
 James Clark (Plymouth Marine Laboratory). Email: jcl@pml.ac.uk
 """
+# Package imports
+from pylag import parallel, processing
+
+# Module imports
+from pylag import version
+from pylag import main
+from pylag import mock
+from pylag import random
+from pylag import grid_metrics
+from pylag import interpolation
+from pylag import math
+from pylag import data_types_python
 
--- pylag/parallel/__init__.py
+++ pylag/parallel/__init__.py
@@ -5,3 +5,4 @@ pylag.parallel
 The `pylag.parallel` subpackage contains modules that make it possible
 to run PyLag in parallel using MPI for Python.
 """
+from pylag.parallel import main
