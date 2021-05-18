7da9be958fda7a2155c24d17d528b5faa97e03e8
James Clark <jcl@pml.ac.uk>
2021-05-18 10:29:43 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import numpy
 
 MAJOR               = 0
 MINOR               = 5
-PATCH               = 0
+PATCH               = 1
 ISRELEASED          = True
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
 
