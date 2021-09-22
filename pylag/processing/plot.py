bb56901194df00a177f55719ec552160bb177416
James Clark <jcl@pml.ac.uk>
2021-09-22 08:39:30 +0100

    Permit plotting of arakawa-a element data

--- pylag/processing/plot.py
+++ pylag/processing/plot.py
@@ -699,15 +699,18 @@ class ArakawaAPlotter(PyLagPlotter):
             if len(field.shape) != 1:
                 raise ValueError('Expected 1D array')
 
-            if field.shape[0] != self.n_nodes:
-                raise ValueError('The size of the `field` array does not match the number of nodes')
+            if field.shape[0] != self.n_nodes and field.shape[0] != self.n_elems:
+                raise ValueError('The size of the `field` array does not match the number of nodes or elements')
 
             _field = field
         else:
             _field = self.preprocess_array(field)
 
         # Compute field value at face centre of ocean triangles
-        _field = _field[self.ocean_simplices].mean(axis=1)
+        if field.shape[0] == self.n_nodes:
+            _field = _field[self.ocean_simplices].mean(axis=1)
+        if field.shape[0] == self.n_elems:
+            _field = _field[self.ocean_elements]
 
         if update is True:
             for collection in ax.collections:
