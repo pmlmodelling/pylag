657cb42514d2b13404b28a552146c59cf96c5a72
James Clark <jcl@pml.ac.uk>
2022-10-13 11:27:15 +0100

    Update serial model factory method

--- pylag/model_factory.py
+++ pylag/model_factory.py
@@ -5,19 +5,32 @@ See Also
 --------
 pylag.parallel.model_factory - Factory method for parallel execution
 """
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
 
 from pylag.model import OPTModel
 from pylag.arakawa_a_data_reader import ArakawaADataReader
 from pylag.fvcom_data_reader import FVCOMDataReader
 from pylag.roms_data_reader import ROMSDataReader
 from pylag.gotm_data_reader import GOTMDataReader
+from pylag.atmosphere_data_reader import AtmosphereDataReader
+from pylag.waves_data_reader import WavesDataReader
+from pylag.composite_data_reader import CompositeDataReader
+from pylag.exceptions import PyLagValueError
 
 # Serial imports
 from pylag.mediator import SerialMediator
 
 
 def get_model(config, datetime_start, datetime_end):
-    """ Factory method for model objects
+    """ Factory method for model objects.
+
+    Configure the OPTModel. Here, the required set of data readers
+    for ocean, wind and waves input data are instantiated and
+    configured. Tests are put in place to ensure a working
+    configuration has been specified.
 
     Parameters
     ----------
@@ -35,24 +48,97 @@ def get_model(config, datetime_start, datetime_end):
      : pylag.model.OPTModel
          Offline particle tracking model object
     """
-    if config.get("OCEAN_CIRCULATION_MODEL", "name") == "ArakawaA":
-        mediator = SerialMediator(config, datetime_start, datetime_end)
-        data_reader = ArakawaADataReader(config, mediator)
-        return OPTModel(config, data_reader)
-    elif config.get("OCEAN_CIRCULATION_MODEL", "name") == "FVCOM":
-        mediator = SerialMediator(config, datetime_start, datetime_end)
-        data_reader = FVCOMDataReader(config, mediator)
-        return OPTModel(config, data_reader)
-    elif config.get("OCEAN_CIRCULATION_MODEL", "name") == "ROMS":
-        mediator = SerialMediator(config, datetime_start, datetime_end)
-        data_reader = ROMSDataReader(config, mediator)
-        return OPTModel(config, data_reader)
-    elif config.get("OCEAN_CIRCULATION_MODEL", "name") == "GOTM":
-        mediator = SerialMediator(config, datetime_start, datetime_end)
-        data_reader = GOTMDataReader(config, mediator)
-        return OPTModel(config, data_reader)
-    else:
-        raise ValueError('Unsupported ocean circulation model.')
+    # Ocean data
+    # ----------
+    try:
+        ocean_product_name = config.get("OCEAN_CIRCULATION_MODEL", "name").strip()
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        ocean_product_name = "None"
 
+    if ocean_product_name == "ArakawaA":
+        ocean_mediator = SerialMediator(config, datetime_start, datetime_end)
+        ocean_data_reader = ArakawaADataReader(config, ocean_mediator)
+    elif ocean_product_name == "FVCOM":
+        ocean_mediator = SerialMediator(config, datetime_start, datetime_end)
+        ocean_data_reader = FVCOMDataReader(config, ocean_mediator)
+    elif ocean_product_name == "ROMS":
+        ocean_mediator = SerialMediator(config, datetime_start, datetime_end)
+        ocean_data_reader = ROMSDataReader(config, ocean_mediator)
+    elif ocean_product_name == "GOTM":
+        ocean_mediator = SerialMediator(config, datetime_start, datetime_end)
+        ocean_data_reader = GOTMDataReader(config, ocean_mediator)
+    elif ocean_product_name.lower() == "none":
+        ocean_data_reader = None
+    else:
+        raise PyLagValueError(f"Unsupported ocean data product "
+                              f"`{ocean_product_name}`")
+
+    # Atmosphere data
+    # ---------------
+    try:
+        atmos_product_name = config.get("ATMOSPHERE_DATA", "source").strip()
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        atmos_product_name = "none"
+
+    if atmos_product_name == "ArakawaA":
+        atmos_mediator = SerialMediator(config, datetime_start,
+                                        datetime_end)
+        atmos_data_reader = AtmosphereDataReader(config, atmos_mediator)
+    elif atmos_product_name.lower() == "none":
+        atmos_data_reader = None
+    else:
+        raise PyLagValueError(f"Unsupported atmosphere data product "
+                              f"`{atmos_product_name}`")
+
+    # Waves data
+    # ----------
+    try:
+        waves_product_name = config.get("WAVES_DATA", "source").strip()
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        waves_product_name = "none"
+
+    if waves_product_name == "ArakawaA":
+        waves_mediator = SerialMediator(config, datetime_start,
+                                        datetime_end)
+        waves_data_reader = WavesDataReader(config, waves_mediator)
+    elif waves_product_name.lower() == "none":
+        waves_data_reader = None
+    else:
+        raise PyLagValueError(f"Unsupported waves data product "
+                              f"`{waves_product_name}`")
+
+    # Error checking
+    # --------------
+    if ocean_data_reader is None and atmos_data_reader is None and \
+            waves_data_reader is None:
+        raise PyLagValueError(f"To build an OPTModel, at least one source "
+                              f"of input data must be specified.")
+
+    if ocean_data_reader is None and atmos_data_reader is not None and \
+            waves_data_reader is not None:
+        raise PyLagValueError(f"At the current time, PyLag does not "
+                              f"support running the model with atmosphere "
+                              f"and waves data without ocean data")
+
+    # Build the OPTModel
+    # ------------------
+    if ocean_data_reader is not None and atmos_data_reader is None and \
+            waves_data_reader is None:
+        opt_model = OPTModel(config, ocean_data_reader)
+    elif ocean_data_reader is None and atmos_data_reader is not None and \
+            waves_data_reader is None:
+        opt_model = OPTModel(config, atmos_data_reader)
+    elif ocean_data_reader is None and atmos_data_reader is None and \
+            waves_data_reader is not None:
+        opt_model = OPTModel(config, waves_data_reader)
+    else:
+        # Using a combination of data types - requires a composite data reader
+        composite_data_reader = CompositeDataReader(config,
+                                                    ocean_data_reader,
+                                                    atmos_data_reader,
+                                                    waves_data_reader)
+        opt_model = OPTModel(config, composite_data_reader)
+
+    return opt_model
 
 __all__ = ['get_model']
