267e382dcd81231b07ae8ced4ab6c322fa9dbb0d
James Clark <jcl@pml.ac.uk>
2022-10-20 19:08:10 +0100

    Further tidy code

--- pylag/main.py
+++ pylag/main.py
@@ -21,9 +21,11 @@ import os
 import sys
 import argparse
 import logging
+import pathlib
 
 from pylag.configuration import get_config
 from pylag.simulator import get_simulator
+from pylag.exceptions import PyLagRuntimeError
 import pylag.random as random
 from pylag import version
 
@@ -31,7 +33,8 @@ from pylag import version
 def main():
     # Parse command line agruments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', help='Path to the configuration file', metavar='')
+    parser.add_argument('-c', '--config',
+                        help='Path to the configuration file', metavar='')
     parsed_args = parser.parse_args(sys.argv[1:])
 
     # Read in run config
@@ -41,14 +44,14 @@ def main():
         print('Failed to create run config. Please make sure a config '\
               'file is given using the -c or --config command line '\
               'arguments.')
-        raise RuntimeError(re.message)
+        raise PyLagRuntimeError(re.message)
 
     # Create output directory if it does not exist already
-    if not os.path.isdir('{}'.format(config.get('GENERAL', 'out_dir'))):
-        os.mkdir('{}'.format(config.get('GENERAL', 'out_dir')))
+    out_dir = config.get('GENERAL', 'out_dir')
+    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True) 
     
     # Initiate logging
-    logging.basicConfig(filename="{}/pylag_out.log".format(config.get('GENERAL', 'out_dir')),
+    logging.basicConfig(filename=f"{out_dir}/pylag_out.log",
                         filemode='w',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -57,10 +60,10 @@ def main():
     
     # Save the version of the code used (current commit + status)
     logger.info('Starting PyLag')
-    logger.info('Using PyLag version: {}'.format(version.version))
+    logger.info(f"Using PyLag version: {version.version}")
     
     # Record configuration to file
-    with open("{}/pylag_out.cfg".format(config.get('GENERAL', 'out_dir')), 'w') as config_out:
+    with open(f"{out_dir}/pylag_out.cfg", 'w') as config_out:
         logger.info('Writing run config to file')
         config.write(config_out)
     
--- pylag/simulator.py
+++ pylag/simulator.py
@@ -19,6 +19,7 @@ from pylag.time_manager import TimeManager
 from pylag.particle_initialisation import get_initial_particle_state_reader
 from pylag.restart import RestartFileCreator
 from pylag.netcdf_logger import NetCDFLogger
+from pylag.exceptions import PyLagValueError, PyLagRuntimeError
 
 from pylag.model_factory import get_model
 
@@ -39,7 +40,7 @@ def get_simulator(config):
     if config.get("SIMULATION", "simulation_type") == "trace":
         return TraceSimulator(config)
     else:
-        raise ValueError('Unsupported simulation type.')  
+        raise PyLagValueError('Unsupported simulation type.')
 
 
 class Simulator(object):
@@ -56,8 +57,8 @@ class Simulator(object):
 class TraceSimulator(Simulator):
     """ Trace simulator
 
-    Simulator for tracing particle pathlines through time. Trace simulators can perform
-    forward or backward in time integrations.
+    Simulator for tracing particle pathlines through time. Trace simulators
+    can perform forward or backward in time integrations.
 
     Parameters
     ----------
@@ -68,11 +69,12 @@ class TraceSimulator(Simulator):
         # Configuration object
         self._config = config
 
-        # Time manager - for controlling particle release events, time stepping etc
+        # Time manager - for controlling particle release events, time stepping
         self.time_manager = TimeManager(self._config)
     
         # Model object
-        self.model = get_model(self._config, self.time_manager.datetime_start, self.time_manager.datetime_end)
+        self.model = get_model(self._config, self.time_manager.datetime_start,
+                               self.time_manager.datetime_end)
 
         # Initial particle state reader
         self.initial_particle_state_reader = get_initial_particle_state_reader(config)
@@ -104,19 +106,20 @@ class TraceSimulator(Simulator):
             self.initial_particle_state_reader.get_particle_data()
 
         if n_particles == len(group_ids):
-            logger.info('Particle seed contains {} '\
-                'particles.'.format(n_particles))
+            logger.info(f'Particle seed contains {n_particles} particles.')
         else:
-            logger.error('Error reading particle initial positions from '\
-                'file. The number of particles specified in the file is '\
-                '{}. The actual number found while parsing the file was '\
-                '{}.'.format(n_particles, len(group_ids)))
+            logger.error(f'Error reading particle initial positions from '
+                         f'file. The number of particles specified in the file '
+                         f'is {n_particles}. The actual number found while '
+                         f'parsing the file was {len(group_ids)}.')
 
-            raise RuntimeError('Error encountered while reading the particle '\
-                'initial positions file. See the log for more details.')
+            raise PyLagRuntimeError('Error encountered while reading the '
+                                    'particle initial positions file. See '
+                                    'the log for more details.')
 
         # Initialise particle arrays
-        self.model.set_particle_data(group_ids, x1_positions, x2_positions, x3_positions)
+        self.model.set_particle_data(group_ids, x1_positions, x2_positions,
+                                     x3_positions)
 
         # Run the ensemble
         run_simulation = True
@@ -128,10 +131,13 @@ class TraceSimulator(Simulator):
             self.model.seed(self.time_manager.time)
             
             # Create data logger
-            file_name = ''.join([self._config.get('GENERAL', 'output_file'), '_{}'.format(self.time_manager.current_release)])
+            file_name = ''.join([self._config.get('GENERAL', 'output_file'),
+                                 f'_{self.time_manager.current_release}'])
             start_datetime = self.time_manager.datetime_start
             grid_names = self.model.get_grid_names()
-            self.data_logger = NetCDFLogger(self._config, file_name, start_datetime, n_particles, grid_names)
+            self.data_logger = NetCDFLogger(self._config, file_name,
+                                            start_datetime, n_particles,
+                                            grid_names)
 
             # Write particle group ids to file
             self.data_logger.write_group_ids(group_ids)
@@ -141,32 +147,38 @@ class TraceSimulator(Simulator):
             self.data_logger.write(self.time_manager.time, particle_diagnostics)
 
             # The main update loop
-            print('\nStarting ensemble member {} ...'.format(self.time_manager.current_release))
+            print(f'\nStarting ensemble member '
+                  f'{self.time_manager.current_release} ...')
             print('Progress:')
-            pbar = ProgressBar(maxval=abs(self.time_manager.time_end), term_width=50).start()
+            pbar = ProgressBar(maxval=abs(self.time_manager.time_end),
+                               term_width=50).start()
             while abs(self.time_manager.time) < abs(self.time_manager.time_end):
                 self.model.update(self.time_manager.time)
                 self.time_manager.update_current_time()
 
                 # Check on status of reading frames and update if necessary
-                # Communicate updated arrays to the data reader if these are out of
-                # date.
+                # Communicate updated arrays to the data reader if these are
+                # out of date.
                 self.model.read_input_data(self.time_manager.time)
 
                 if self.time_manager.write_output_to_file() == 1:
-                    particle_diagnostics = self.model.get_diagnostics(self.time_manager.time)
-                    self.data_logger.write(self.time_manager.time, particle_diagnostics)
+                    particle_diagnostics = self.model.get_diagnostics(
+                            self.time_manager.time)
+                    self.data_logger.write(self.time_manager.time,
+                                           particle_diagnostics)
                 
                 if self.time_manager.sync_data_to_disk() == 1:
                     self.data_logger.sync()
 
                 if self.restart_creator:
                     if self.time_manager.create_restart_file() == 1:
-                        file_name_stem = 'restart_{}'.format(self.time_manager.current_release)
+                        file_name_stem = f'restart_{self.time_manager.current_release}'
                         datetime_current = self.time_manager.datetime_current
                         particle_data = self.model.get_particle_data()
 
-                        self.restart_creator.create(file_name_stem, n_particles, datetime_current, particle_data)
+                        self.restart_creator.create(file_name_stem, n_particles,
+                                                    datetime_current,
+                                                    particle_data)
 
                 pbar.update(abs(self.time_manager.time))
             pbar.finish()
@@ -179,8 +191,9 @@ class TraceSimulator(Simulator):
                 run_simulation = True
 
                 # Set up data access for the new simulation
-                self.model.setup_input_data_access(self.time_manager.datetime_start,
-                                                   self.time_manager.datetime_end)
+                self.model.setup_input_data_access(
+                        self.time_manager.datetime_start,
+                        self.time_manager.datetime_end)
             else:
                 run_simulation = False
 
