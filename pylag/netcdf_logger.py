ee73d93273b060cac0802eec8c20f86bdbd8383a
James Clark <jcl@pml.ac.uk>
2022-10-20 08:30:08 +0100

    Tidy logger module

--- pylag/netcdf_logger.py
+++ pylag/netcdf_logger.py
@@ -16,6 +16,7 @@ from pylag.data_types_python import DTYPE_INT, DTYPE_FLOAT
 from pylag.data_types_python import INT_INVALID, FLOAT_INVALID
 
 from pylag import variable_library
+from pylag.exceptions import PyLagValueError
 
 
 class NetCDFLogger(object):
@@ -41,7 +42,8 @@ class NetCDFLogger(object):
     grid_names : list[str]
         List of grids on which input data is defined.
     """
-    def __init__(self, config, file_name, start_datetime, n_particles, grid_names):
+    def __init__(self, config, file_name, start_datetime, n_particles,
+                 grid_names):
         
         logger = logging.getLogger(__name__)
 
@@ -52,32 +54,33 @@ class NetCDFLogger(object):
         else:
             self.file_name = ''.join([file_name, '.nc'])
 
-        try:
-            logger.info('Creating output file: {}.'.format(self.file_name))
-            self._ncfile = Dataset(self.file_name, mode='w', format='NETCDF4')
-        except:
-            raise RuntimeError('Failed to create output file {}.'.format(self.file_name))
+        logger.info(f'Creating output file: {self.file_name}.')
+        self._ncfile = Dataset(self.file_name, mode='w', format='NETCDF4')
 
         # Compression options for the netCDF variables.
         self._ncopts = {'zlib': True, 'complevel': 7}
 
         # Time units
-        self._simulation_time_units = 'seconds since {}'.format(start_datetime)
+        self._simulation_time_units = f'seconds since {start_datetime}'
 
         # Read in the coordinate system
-        coordinate_system = self.config.get("OCEAN_CIRCULATION_MODEL", "coordinate_system").strip().lower()
+        coordinate_system = self.config.get("OCEAN_CIRCULATION_MODEL",
+                                            "coordinate_system").strip().lower()
         if coordinate_system in ["cartesian", "geographic"]:
             self.coordinate_system = coordinate_system
         else:
-            raise ValueError("Unsupported model coordinate system `{}'".format(coordinate_system))
+            raise PyLagValueError(f"Unsupported model coordinate "
+                                  f"system `{coordinate_system}'")
 
         # Local environmental variables
         self._env_vars = {}
 
         # Add environmental variables, as requested
         try:
-            var_names = self.config.get("OUTPUT", "environmental_variables").strip().split(',')
-            self.environmental_variables = [var_name.strip() for var_name in var_names]
+            var_names = self.config.get("OUTPUT",
+                    "environmental_variables").strip().split(',')
+            self.environmental_variables = \
+                    [var_name.strip() for var_name in var_names]
         except (configparser.NoSectionError, configparser.NoOptionError) as e:
             self.environmental_variables = []
             pass
@@ -106,75 +109,93 @@ class NetCDFLogger(object):
         self._time.long_name = 'Time'
 
         # Add particle group ids
-        self._group_id = self._ncfile.createVariable('group_id', DTYPE_INT, ('particles',))
+        self._group_id = self._ncfile.createVariable('group_id', DTYPE_INT,
+                                                     ('particles',))
         self._group_id.long_name = 'Particle group ID'
 
         # x1
-        x1_var_name = variable_library.get_coordinate_variable_name(self.coordinate_system, 'x1')
+        x1_var_name = variable_library.get_coordinate_variable_name(
+                self.coordinate_system, 'x1')
         self._x1 = self._ncfile.createVariable(x1_var_name,
-                                               variable_library.get_data_type(x1_var_name),
-                                               ('time', 'particles',), **self._ncopts)
+                variable_library.get_data_type(x1_var_name),
+                ('time', 'particles',), **self._ncopts)
         self._x1.units = variable_library.get_units(x1_var_name)
         self._x1.long_name = variable_library.get_long_name(x1_var_name)
 
         # x2
-        x2_var_name = variable_library.get_coordinate_variable_name(self.coordinate_system, 'x2')
+        x2_var_name = variable_library.get_coordinate_variable_name(
+                self.coordinate_system, 'x2')
         self._x2 = self._ncfile.createVariable(x2_var_name,
-                                               variable_library.get_data_type(x2_var_name),
-                                               ('time', 'particles',), **self._ncopts)
+                variable_library.get_data_type(x2_var_name),
+                ('time', 'particles',), **self._ncopts)
         self._x2.units = variable_library.get_units(x2_var_name)
         self._x2.long_name = variable_library.get_long_name(x2_var_name)
 
         # x3
-        x3_var_name = variable_library.get_coordinate_variable_name(self.coordinate_system, 'x3')
+        x3_var_name = variable_library.get_coordinate_variable_name(
+                self.coordinate_system, 'x3')
         self._x3 = self._ncfile.createVariable(x3_var_name,
-                                               variable_library.get_data_type(x3_var_name),
-                                               ('time', 'particles',), **self._ncopts)
+                variable_library.get_data_type(x3_var_name),
+                ('time', 'particles',), **self._ncopts)
         self._x3.units = variable_library.get_units(x3_var_name)
         self._x3.long_name = variable_library.get_long_name(x3_var_name)
 
         # Add host
         for grid_name in self.grid_names:
-            self._host_vars[grid_name] = self._ncfile.createVariable('host_{}'.format(grid_name), DTYPE_INT,
-                                                                     ('time', 'particles',), **self._ncopts)
+            self._host_vars[grid_name] = self._ncfile.createVariable(
+                    f'host_{grid_name}', DTYPE_INT,
+                    ('time', 'particles',), **self._ncopts)
             self._host_vars[grid_name].units = 'None'
-            self._host_vars[grid_name].long_name = 'Host horizontal element on grid {}'.format(grid_name)
-            self._host_vars[grid_name].invalid = '{}'.format(INT_INVALID)
+            self._host_vars[grid_name].long_name = (f"Host horizontal element "
+                                                    f"on grid {grid_name}")
+            self._host_vars[grid_name].invalid = f"{INT_INVALID}"
         
         # Add status variables
-        self._in_domain = self._ncfile.createVariable('in_domain', 'i4', ('time', 'particles',), **self._ncopts)
+        self._in_domain = self._ncfile.createVariable('in_domain', 'i4',
+                                                      ('time', 'particles',),
+                                                      **self._ncopts)
         self._in_domain.units = 'None'
         self._in_domain.long_name = 'In domain flag (1 - yes; 0 - no)'
 
-        self._status = self._ncfile.createVariable('status', 'i4', ('time', 'particles',), **self._ncopts)
+        self._status = self._ncfile.createVariable('status', 'i4',
+                                                   ('time', 'particles',),
+                                                   **self._ncopts)
         self._status.units = 'None'
         self._status.long_name = 'Status flag (1 - error state; 0 - ok)'
 
-        self._is_beached = self._ncfile.createVariable('is_beached', DTYPE_INT, ('time', 'particles',), **self._ncopts)
+        self._is_beached = self._ncfile.createVariable('is_beached',
+                                                       DTYPE_INT,
+                                                       ('time', 'particles',),
+                                                       **self._ncopts)
         self._is_beached.long_name = 'Is beached'
 
-        self._age = self._ncfile.createVariable('age', variable_library.get_data_type('age'),
-                                                ('time', 'particles',), **self._ncopts)
+        self._age = self._ncfile.createVariable('age',
+                variable_library.get_data_type('age'),
+                ('time', 'particles',), **self._ncopts)
         self._age.units = variable_library.get_units('age')
         self._age.long_name = variable_library.get_long_name('age')
-        self._age.invalid = '{}'.format(variable_library.get_invalid_value('age'))
+        self._age.invalid = f"{variable_library.get_invalid_value('age')}"
 
-        self._is_alive = self._ncfile.createVariable('is_alive', 'i4', ('time', 'particles',), **self._ncopts)
+        self._is_alive = self._ncfile.createVariable('is_alive', 'i4',
+                                                     ('time', 'particles',),
+                                                     **self._ncopts)
         self._is_alive.units = 'None'
         self._is_alive.long_name = 'Is alive flag (1 - yes; 0 - no)'
 
         # Add grid variables
-        self._h = self._ncfile.createVariable('h', variable_library.get_data_type('h'),
-                                              ('time', 'particles',), **self._ncopts)
+        self._h = self._ncfile.createVariable('h',
+                variable_library.get_data_type('h'),
+                ('time', 'particles',), **self._ncopts)
         self._h.units = variable_library.get_units('h')
         self._h.long_name = variable_library.get_long_name('h')
         self._h.invalid = '{}'.format(variable_library.get_invalid_value('h'))
         
-        self._zeta = self._ncfile.createVariable('zeta', variable_library.get_data_type('zeta'),
-                                                 ('time', 'particles',), **self._ncopts)
+        self._zeta = self._ncfile.createVariable('zeta',
+                variable_library.get_data_type('zeta'),
+                ('time', 'particles',), **self._ncopts)
         self._zeta.units = variable_library.get_units('zeta')
         self._zeta.long_name = variable_library.get_long_name('zeta')
-        self._zeta.invalid = '{}'.format(variable_library.get_invalid_value('zeta'))
+        self._zeta.invalid = f"{variable_library.get_invalid_value('zeta')}"
 
         for var_name in self.environmental_variables:
             data_type = variable_library.get_data_type(var_name)
@@ -182,10 +203,11 @@ class NetCDFLogger(object):
             long_name = variable_library.get_long_name(var_name)
             invalid = variable_library.get_invalid_value(var_name)
 
-            self._env_vars[var_name] = self._ncfile.createVariable(var_name, data_type, ('time', 'particles',), **self._ncopts)
+            self._env_vars[var_name] = self._ncfile.createVariable(var_name,
+                    data_type, ('time', 'particles',), **self._ncopts)
             self._env_vars[var_name].units = units
             self._env_vars[var_name].long_name = long_name
-            self._env_vars[var_name].invalid = '{}'.format(invalid)
+            self._env_vars[var_name].invalid = f'{invalid}'
 
     def write_group_ids(self, group_ids):
         """ Write particle group IDs to file
@@ -210,7 +232,8 @@ class NetCDFLogger(object):
             The current time
 
         particle_data : dict
-            Dictionary containing particle data with format ['attribute': [...]].
+            Dictionary containing particle data with format
+            ['attribute': [...]].
 
         Returns
         -------
@@ -236,7 +259,8 @@ class NetCDFLogger(object):
 
         # Add host horizontal elements
         for grid_name in self.grid_names:
-            self._host_vars[grid_name][tidx, :] = particle_data['host_{}'.format(grid_name)]
+            self._host_vars[grid_name][tidx, :] = \
+                    particle_data[f'host_{grid_name}']
 
         # Add environmental variables
         for var_name in self.environmental_variables:
